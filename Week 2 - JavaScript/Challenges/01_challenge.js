e94af23b31a7d15d1fec3f404caf8a74bfe7393e
sourabhjaishwal <jaishwalsourabh2000@gmail.com>
2025-05-23 22:31:53 +0530

    challenge on variables

---
+++ Week 2 - JavaScript/Challenges/01_challenge.js
@@ -0,0 +1,15 @@
+/*
+1. Create a variable for each of the following. 
+
+- Your favorite color, your height in centimeters, and whether you like pizza. 
+- Use appropriate variable declarations (`let`, `const`, or `var`). 
+- Try logging it using `console.log`
+*/
+
+var favColor = "Blue";
+let height = 178.323;
+const likePizza = true;
+
+console.log(favColor); // Blue
+console.log(height); // 178.323
+console.log(likePizza); // true
