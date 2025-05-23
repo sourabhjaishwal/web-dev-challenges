1e92fda46f9fd72acea4317ee688778d5c6bed69
sourabhjaishwal <jaishwalsourabh2000@gmail.com>
2025-05-23 22:33:12 +0530

    if/else statement

---
+++ Week 2 - JavaScript/Challenges/03_challenge.js
@@ -0,0 +1,15 @@
+/*
+1. Write an if/else statement that checks if a number is even or odd.
+
+- If it's even, print "The number is even." Otherwise, print "The number is odd."
+*/
+
+let num = 5;
+// declare a variable `num` and assign it a value of 5
+
+// if the number is divisible by 2, it's the number is even else it's odd
+if (num % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
